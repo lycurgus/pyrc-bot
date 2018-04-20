ed49a9c00684b965c54585047d6ac18bc4bd0ea2
lycurgus <lycurgus@lycr.gs>
2018-04-21 09:52:49 +1000

    tone down the 'maximum' experience

--- modules/maximum.py
+++ modules/maximum.py
@@ -4,10 +4,13 @@ parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 
 import module
+import util
 
 @module.regex(r"(?:maximum )?rabbit")
 @module.type("PRIVMSG")
 def max_rabbit(bot,message,regex_matches=None):
+	if util.chance(0.9):
+		return
 	bot.commands.privmsg(message.replyto,"ᴍᴀxɪᴍᴜᴍ ʀᴀʙʙɪᴛ")
 
 maximum = module.Module("maximum")
