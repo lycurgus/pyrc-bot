1b8e568a651740a06bba23201a633c29bc8ae29e
lycurgus <lycurgus@lycr.gs>
2018-04-21 18:06:48 +1000

    move the discordian time function out of util.py into the relevant module

--- modules/discordian.py
+++ modules/discordian.py
@@ -7,11 +7,40 @@ import module
 from ddate.base import DDate
 from datetime import datetime
 from datetime import timedelta
+from time import time
+
+def disco_time():
+	#THUD is 5 THUD minutes long = 115 real minutes
+	#one THUD minute = 23 THUD seconds = 23 real minutes (1 THUD second = 1 real minute)
+	#after THUD each hour = 5 minutes (real and discordian). 1 discordian second = 12 real seconds
+	now = int(time())
+	#print("now: {}".format(now))
+	since_midnight = now % days(1)
+	#print("seconds since midnight: {}".format(since_midnight))
+	discordian_day_start = hours(5) + minutes(23)
+	#print("day start is: {}".format(discordian_day_start))
+	thud_length = minutes(115)
+	#print("thud length is: {}".format(thud_length))
+	discordian_timestamp = (since_midnight - discordian_day_start + hours(24)) % hours(24)
+	#print("discordian_timestamp: {}".format(discordian_timestamp))
+	thud_p = discordian_timestamp < thud_length
+	if thud_p:
+		hour = "THUD" #constant
+		minute = (discordian_timestamp // minutes(23)) #0-4
+		second = ((discordian_timestamp // 60)) % 23 #0-22
+	else:
+		post_thud_timestamp = discordian_timestamp - thud_length
+		#print("time after THUD: {}".format(post_thud_timestamp))
+		hour = (post_thud_timestamp // minutes(5)) #0-264
+		minute = ((post_thud_timestamp - (hour * minutes(5))) // minutes(1)) #0-4
+		second = ((post_thud_timestamp // 12) % 5) #0-4
+	time_str = "it is {}:{}:{}".format(hour,minute,second)
+	return time_str
 
 @module.direct
 @module.regex(r"what time is it\?")
 def discord_time(bot,message,regex_matches=None):
-	bot.commands.privmsg(message.replyto,discord_time())
+	bot.commands.privmsg(message.replyto,disco_time())
 
 @module.direct
 @module.regex(r"what day is it\?")
--- util.py
+++ util.py
@@ -95,35 +95,6 @@ def hours(h):
 def minutes(m):
 	return 60 * m
 
-def discord_time():
-	from time import time
-	#THUD is 5 THUD minutes long = 115 real minutes
-	#one THUD minute = 23 THUD seconds = 23 real minutes (1 THUD second = 1 real minute)
-	#after THUD each hour = 5 minutes (real and discordian). 1 discordian second = 12 real seconds
-	now = int(time())
-	#print("now: {}".format(now))
-	since_midnight = now % days(1)
-	#print("seconds since midnight: {}".format(since_midnight))
-	discordian_day_start = hours(5) + minutes(23)
-	#print("day start is: {}".format(discordian_day_start))
-	thud_length = minutes(115)
-	#print("thud length is: {}".format(thud_length))
-	discordian_timestamp = (since_midnight - discordian_day_start + hours(24)) % hours(24)
-	#print("discordian_timestamp: {}".format(discordian_timestamp))
-	thud_p = discordian_timestamp < thud_length
-	if thud_p:
-		hour = "THUD" #constant
-		minute = (discordian_timestamp // minutes(23)) #0-4
-		second = ((discordian_timestamp // 60)) % 23 #0-22
-	else:
-		post_thud_timestamp = discordian_timestamp - thud_length
-		#print("time after THUD: {}".format(post_thud_timestamp))
-		hour = (post_thud_timestamp // minutes(5)) #0-264
-		minute = ((post_thud_timestamp - (hour * minutes(5))) // minutes(1)) #0-4
-		second = ((post_thud_timestamp // 12) % 5) #0-4
-	time_str = "it is {}:{}:{}".format(hour,minute,second)
-	return time_str
-
 def randomly_replace_vowels(word,count=None):
 	import random
 	c = count
