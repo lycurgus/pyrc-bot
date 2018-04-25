a2bbebc7a8bdabaa02ccef3015f50d6b538f45e5
lycurgus <lycurgus@lycr.gs>
2018-04-25 16:47:22 +1000

    add a small module for debugging channel users

---
+++ modules/debug.py
@@ -0,0 +1,26 @@
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+
+import module
+
+@module.type("PRIVMSG")
+@module.regex(r"check users (.*)")
+def users_check(bot,message,regex_matches=None):
+	channel = regex_matches.group(1).strip()
+	if channel in bot.channels.keys():
+		print(" ".join(bot.channels[channel].users.keys()))
+		print("count: {}".format(len(bot.channels[channel].users.keys())))
+
+@module.type("PRIVMSG")
+@module.regex(r"is (.*) in (.*)")
+def user_check(bot,message,regex_matches=None):
+	user = regex_matches.group(1).strip()
+	channel = regex_matches.group(2).strip()
+	if channel in bot.channels.keys():
+		print(True if user in bot.channels[channel].users.keys() else False)
+
+debug = module.Module("debug")
+debug.add_function(users_check)
+debug.add_function(user_check)
