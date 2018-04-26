ff125a0a987cbbcd0fc0a6c5d2be46f8ff5d83d0
lycurgus <lycurgus@lycr.gs>
2018-04-26 20:53:44 +1000

    check if main directory is in path before inserting it - at least for one module

--- modules/westworld.py
+++ modules/westworld.py
@@ -1,8 +1,8 @@
 import os,sys,inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
-#TODO check if parent directory already in path before inserting
-sys.path.insert(0,parentdir)
+if parentdir not in sys.path:
+	sys.path.insert(0,parentdir)
 import module
 
 @module.direct
