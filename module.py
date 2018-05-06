f76d0dcef7cdd59b13697ea4dbabbc28777c19c5
lycurgus <lycurgus@lycr.gs>
2018-05-06 21:29:25 +1000

    clean up some spare code from the line/message merger

--- module.py
+++ module.py
@@ -3,8 +3,6 @@ from functools import wraps
 import re
 import datetime
 
-
-
 def check_function(function,message,bot):
 	match = None
 	if not message.message:
@@ -69,7 +67,6 @@ def check_function(function,message,bot):
 				match = at.match(message.message)
 				if not match: return (False,None)
 	return (True,match)
-
 
 class Timeout:
 	def __init__(self,name,days=0,hours=0,minutes=0,seconds=0):
@@ -152,108 +149,11 @@ class Module:
 		if self.setup_function:
 			self.setup_function(bot)
 
-	def check_triggers(self,message,bot):
-		timeout = ""
-		matches = []
-		for function in self.functions:
-			checks = []
-			checknames = []
-			match = None
-			if not message.message:
-				return ()
-			#print("function {} has attributes {}".format(function.__name__," ".join([attrib for attrib in dir(function) if not attrib.startswith("__")])))
-			check_attributes = list(set(Module.triggers).intersection(dir(function)))
-			if "direct" in check_attributes:
-				check_attributes.remove("direct")
-				check_attributes.insert(0,"direct")
-			for a in check_attributes:
-				if hasattr(function,a):
-					checknames.append(a)
-					at = getattr(function,a,None)
-					if a == "direct":
-						checks.append(bot.being_addressed(message.line))
-						if any([message.message.startswith(bn.lower()) for bn in bot.names]):
-							print('original message: {}'.format(message.original))
-							message.message = re.sub(r'{}(?:[:, ])?'.format('|'.join(bot.names)),'',message.original,count=1,flags=re.IGNORECASE)
-							print('altered message: {}'.format(message.message))
-					elif a == "timeout":
-						timeout = at
-						expired = bot.timeout(at).get()
-						checks.append(expired)
-					elif a == "type":
-						checks.append(message.command in at)
-					elif a == "sender":
-						if message.sender:
-							checks.append(message.sender.lower() in util.lower(at))
-						else:
-							checks.append(False)
-					elif a == "sender-not":
-						if message.sender:
-							checks.append(message.sender.lower() not in util.lower(at))
-						else:
-							checks.append(True)
-					elif a == "sender-admin":
-						checks.append(bot.is_admin(message.sender))
-					elif a == "sender-not-admin":
-						checks.append(not bot.is_admin(message.sender))
-					elif a == "channel":
-						checks.append(message.channel in at)
-					elif a == "channel-not":
-						checks.append(message.channel not in at)
-					elif a == "user-present":
-						if message.channel:
-							checks.append(any([u.lower() in util.lower(bot.channels[message.channel].users.keys()) for u in at]))
-						else:
-							checks.append(False)
-					elif a == "user-not-present":
-						if message.channel:
-							present = []
-							for u in at:
-								user_found = False
-								for cu in bot.channels[message.channel].users.keys():
-									if cu.lower() == u.lower():
-										user_found = True
-								present.append(user_found)
-							checks.append(not any(present))
-						elif message.sender:
-							if message.sender.lower() in util.lower(at):
-								checks.append(False)
-						else:
-							checks.append(True)
-					elif a == "mention":
-						checks.append(any([bn in message.message for bn in bot.names]))
-					elif a == "address":
-						checks.append(any([any((message.message.startswith(bn),line.rest.endswith(bn))) for bn in bot.names]))
-					elif a == "action":
-						#print("action original: {}".format(repr(message.original)))
-						#action = re.match(r"^\x01(.*)\x01$",message.original)
-						action = message.is_ctcp and message.original.upper().startswith("ACTION")
-						if action:
-							checks.append(True)
-							message.message = re.sub(r'^ACTION ','',message.original,count=1)
-						else:
-							checks.append(False)
-					elif a == "ctcp":
-						checks.append(True if message.is_ctcp else False)
-					elif a == "regex":
-						match = at.match(message.message)
-						checks.append(True if match else False)
-					elif a == "disable":
-						checks.append(False)
-			if all(checks):
-				if timeout != "":
-					if bot.timeout(timeout).get():
-						bot.timeout(timeout).set()
-				matches.append((function,match))
-		return matches
-
 	def check_functions(self,message,bot):
 		for function in self.functions:
 			passed,match = check_function(function,message,bot)
 			if passed:
 				function(bot,message,match)
-
-
 
 	def check_timers(self,bot,tick_num,delta):
 		for timer in self.timers:
